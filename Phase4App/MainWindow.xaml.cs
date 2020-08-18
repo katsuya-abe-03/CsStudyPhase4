845a78ecf93b9a12120619bff2728cfc780db2a6
katsuya-abe-03 <katsuya_abe@sobal.co.jp>
2020-08-18 14:40:24 +0900

    実装完了

---
+++ Phase4App/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Phase4App
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadSettings();
+        }
+
+        private void LoadSettings()
+        {
+            tbImagePath.Text = Properties.Settings.Default.SelectImagePath;
+            Left             = Properties.Settings.Default.MainWndLeft;
+            Top              = Properties.Settings.Default.MainWndTop;
+            Width            = Properties.Settings.Default.MainWndWidth;
+            Height           = Properties.Settings.Default.MainWndHeight;
+        }
+
+        private void SaveSettings()
+        {
+            Properties.Settings.Default.SelectImagePath = tbImagePath.Text;
+            Properties.Settings.Default.MainWndLeft     = Left;
+            Properties.Settings.Default.MainWndTop      = Top;
+            Properties.Settings.Default.MainWndWidth    = Width;
+            Properties.Settings.Default.MainWndHeight   = Height;
+            Properties.Settings.Default.Save();
+        }
+
+        private void MainWindowClosing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            SaveSettings();
+        }
+
+        private void TbImageSizeRateMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            //最終的に0.1刻みにするために、10倍で演算。0.1刻みにしないと、切り上げられてしまい、画面に納める意図から外れてしまう。
+            //(scrollViewer.ExtentWidth / SldImageSizeRate.Value)で1.0倍のサイズを算出
+            //scrollViewer.ActualWidth * 10 / (scrollViewer.ExtentWidth / SldImageSizeRate.Value)を変換し、割り算削減
+            int rateWidth = (int)(scrollViewer.ActualWidth * 10 * SldImageSizeRate.Value / scrollViewer.ExtentWidth);
+
+            //高さも同様に演算
+            int rateHeight = (int)(scrollViewer.ActualHeight * 10 * SldImageSizeRate.Value / scrollViewer.ExtentHeight);
+
+            SldImageSizeRate.Value = (double)Math.Min(rateWidth,rateHeight)/ 10;
+        }
+    }
+}
---
+++ Phase4App/SizeStrConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Phase4App
+{
+    public class SizeStrConverter : IValueConverter
+{
+    //仕様で1～3のため、先頭に空文字
+    private static readonly string[] sizeStr = {"", "Small","Middle","Large" };
+
+    public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+    {
+        return sizeStr[(byte)(double)value];
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+    {
+        return null;
+    }
+}
+}
